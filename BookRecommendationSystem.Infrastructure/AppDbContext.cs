13b76bb01f617f15665c0bfcc19663aa84f15e4b
SmurW <smurw.developer@gmail.com>
2025-01-31 13:42:26 +1000

    Созданы репозитории и контекст БД, реализован репозиторий автора

---
+++ BookRecommendationSystem.Infrastructure/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookRecommendationSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookRecommendationSystem.Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Author> Authors { get; set; } = null!;
+        public DbSet<Book> Books { get; set; } = null!;
+        public DbSet<Genre> Genres { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
---
+++ BookRecommendationSystem.Infrastructure/Repositories/AuthorRepository.cs
@@ -0,0 +1,52 @@
+﻿using BookRecommendationSystem.Domain.Entities;
+using BookRecommendationSystem.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookRecommendationSystem.Infrastructure.Repositories
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+        public readonly AppDbContext _context;
+        public AuthorRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Author author)
+        {
+            await _context.AddAsync(author);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var author = GetByIdAsync(id).Result;
+            if (author != null)
+            {
+                _context.Authors.Remove(author);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Author>> GetAllAsync()
+        {
+            return await _context.Authors.ToListAsync();
+        }
+
+        public async Task<Author> GetByIdAsync(Guid id)
+        {
+            return await _context.Authors.FindAsync(id);
+        }
+
+        public async Task<Author> GetByNameAsync(string name)
+        {
+            return await _context.Authors.FirstAsync(x => x.Name == name);
+        }
+
+        public async Task UpdateAsync(Author author)
+        {
+            _context.Authors.Update(author);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
