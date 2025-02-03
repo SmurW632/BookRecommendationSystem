dc30061fb2d6f3545986ff747f1d735a9b58654d
SmurW <smurw.developer@gmail.com>
2025-02-03 10:00:12 +1000

    Реализована система кастомных исключений

---
+++ BookRecommendationSystem.Domain/ExceptionMessageConsts/ExMesConsts.cs
@@ -0,0 +1,13 @@
+﻿namespace BookRecommendationSystem.Domain.ExceptionMessageConsts
+{
+    public class ExMesConsts
+    {
+        public const string BOOK_NOT_FOUND = "Книга не найдена.";
+        public const string RATING_NOT_FOUND = "Рейтинг не найден.";
+        public const string RECOMMENDATION_NOT_FOUND = "Рекомендация не найдена.";
+        public const string AUTHOR_NOT_FOUND = "Автор не найден.";
+        public const string GENRE_NOT_FOUND = "Жанр не найден.";
+        public const string ID_IS_ZERO = "ID равен 0";
+        public const string USER_NOT_FOUND = "Пользователь не найден.";
+    }
+}
