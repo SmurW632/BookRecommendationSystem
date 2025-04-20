105ba7ffb26186db57e8a698e1b346e5428899dd
SmurW <smurw.developer@gmail.com>
2025-04-20 18:29:44 +1000

    Добавлено поле Email в сущность Customer

--- BookRecommendationSystem.Domain/Modules/Customers/Customer.cs
+++ BookRecommendationSystem.Domain/Modules/Customers/Customer.cs
@@ -12,6 +12,10 @@ namespace BookRecommendationSystem.Domain.Modules.Customers
         [StringLength(20, ErrorMessage = "Поле должно содержать до 20 символов")]
         public string Nickname { get; set; } = string.Empty;
 
+        [Required]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Поле Email не корректено")]
+        public string Email { get; set; } = string.Empty;
+
         [StringLength(100, ErrorMessage = "Поле должно содержать до 100 символов")]
         public string FirstName { get; set; } = string.Empty;
         [StringLength(100, ErrorMessage = "Поле должно содержать до 100 символов")]
